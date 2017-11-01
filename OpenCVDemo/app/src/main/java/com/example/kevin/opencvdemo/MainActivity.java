8fd953d7151651b76b812062c13ca0bd63cd72d5
Kevin Huang <khuan042@uottawa.ca>
2017-10-31 22:31:25 -0400

    updated file

--- OpenCVDemo/app/src/main/java/com/example/kevin/opencvdemo/MainActivity.java
+++ OpenCVDemo/app/src/main/java/com/example/kevin/opencvdemo/MainActivity.java
@@ -2,11 +2,19 @@ package com.example.kevin.opencvdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     static {
-
+        if(OpenCVLoader.initDebug()){
+            Log.d(TAG, "Opencv successfully loaded");
+        }
+        else{
+            Log.d(TAG, "Opencv not loaded");
+        }
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
