00070e363c5a3948db20d854a7b72bb9664b883b
Kevin Huang <khuan042@uottawa.ca>
2017-10-31 20:53:11 -0400

    imported OpenCV library

--- OpenCVDemo/app/src/main/java/com/example/kevin/opencvdemo/MainActivity.java
+++ OpenCVDemo/app/src/main/java/com/example/kevin/opencvdemo/MainActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    static {
 
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
